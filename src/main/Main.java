416e4005878c98ba04d145e78fee6f2bbb4844d7
I841311 <I841311@SLET00567336A.amer.global.corp.sap>
2016-06-22 23:02:45 -0300

    main

--- src/main/Main.java
+++ src/main/Main.java
@@ -19,26 +19,26 @@ import main.security.JasyptEncryptor;
 public class Main {
 	private static RepositoryFactory factory;
 	
-	private static final HashMap<String, String> corsHeaders = new HashMap<String, String>();
-    
-    static {
-        corsHeaders.put("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-        corsHeaders.put("Access-Control-Allow-Origin", "*");
-        corsHeaders.put("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,");
-        corsHeaders.put("Access-Control-Allow-Credentials", "true");
-    }
-    
-    public final static void apply() {
-        Filter filter = new Filter() {
-            @Override
-            public void handle(Request request, Response response) throws Exception {
-                corsHeaders.forEach((key, value) -> {
-                    response.header(key, value);
-                });
-            }
-        };
-        Spark.after(filter);
-    }
+//	private static final HashMap<String, String> corsHeaders = new HashMap<String, String>();
+//    
+//    static {
+//        corsHeaders.put("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+//        corsHeaders.put("Access-Control-Allow-Origin", "*");
+//        corsHeaders.put("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With,Content-Length,Accept,Origin,");
+//        corsHeaders.put("Access-Control-Allow-Credentials", "true");
+//    }
+//    
+//    public final static void apply() {
+//        Filter filter = new Filter() {
+//            @Override
+//            public void handle(Request request, Response response) throws Exception {
+//                corsHeaders.forEach((key, value) -> {
+//                    response.header(key, value);
+//                });
+//            }
+//        };
+//        Spark.after(filter);
+//    }
 
 	public static void main(String... arguments) {
         new Main().startSparkServer();
@@ -69,7 +69,7 @@ public class Main {
     private void setUpRoutes() {
     	Dependencies dependencies = buildDependencies();
     	
-    	Main.apply();
+//    	Main.apply();
     	
         get("/read-user", new ReadUserRoute(dependencies));
         post("/login", new LoginRoute(dependencies));

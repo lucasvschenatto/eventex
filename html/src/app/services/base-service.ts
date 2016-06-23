1639957027f2d98b7abb725028ecdf393d9ae071
Luiz Felipe Bertoldi de Oliveira <luiz.oliveira@cwi.com.br>
2016-06-23 02:41:25 -0300

    Commit de arquivos ignorados.

--- html/src/app/services/base-service.ts
+++ html/src/app/services/base-service.ts
@@ -3,6 +3,6 @@ export class BaseService {
     protected _baseUrl: string;
 
     constructor() {
-        this._baseUrl = "http://http://eventex.herokuapp.com";
+        this._baseUrl = "http://eventex.herokuapp.com";
     }
 }
